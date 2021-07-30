b5ab7238b24d09ede1339441ddb21002aace027b
Jack Wright <jack.wright@aimlab.com>
2021-07-30 17:26:17 +1000

    serial: Added uart extension commands for stm32h7
    Added extension commands to control stm32h7 receive timeout, driver enable,
    and receiver idle interrupt request
    These commands translate directly to stm32 ll hal
    They can be used to extend application functionality for serial drivers,
    including using hardware driver enable pin and idle/receive timeout for
    half duplex rs485 transceivers.
    
    The source file is currently hacked into the STM32 UART KConfig,
    to be fixed in future
    
    Signed-off-by: Jack Wright <jack.wright@aimlab.com>

---
+++ include/drivers/serial/stm32h7xx_uart.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2021 Aim Lab Automation Technologies
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Extended public API for STM32H7xx UART 
+ *
+ * The receive timeout and Driver Enable Mode apis are 
+ * exposed in this file
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_SERIAL_STM32H7XX_UART_H_
+#define ZEPHYR_INCLUDE_DRIVERS_SERIAL_STM32H7XX_UART_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <device.h>
+#include <drivers/uart.h>
+#include <stm32h7xx_ll_usart.h>
+
+
+void uart_stm32h7xx_receive_timeout_enable(const struct device *dev);
+void uart_stm32h7xx_receive_timeout_disable(const struct device *dev);
+int uart_stm32h7xx_receive_timeout_ready(const struct device *dev);
+void uart_stm32h7xx_receive_timeout_set(const struct device *dev, uint32_t _timeout);
+
+#ifndef CONFIG_UART_ASYNC_API
+void uart_stm32h7xx_irq_idle_enable(const struct device *dev);
+void uart_stm32h7xx_irq_idle_disable(const struct device *dev);
+int uart_stm32h7xx_irq_idle_ready(const struct device *dev);
+#endif
+
+void uart_stm32h7xx_dem_enable(const struct device *dev);
+void uart_stm32h7xx_dem_disable(const struct device *dev);
+void uart_stm32h7xx_dem_at_set(const struct device *dev, uint32_t _at);
+void uart_stm32h7xx_dem_dt_set(const struct device *dev, uint32_t _dt);
+void uart_stm32h7xx_dem_pol_set(const struct device *dev, uint32_t _pol);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_SERIAL_STM32H7XX_UART_H_ */
