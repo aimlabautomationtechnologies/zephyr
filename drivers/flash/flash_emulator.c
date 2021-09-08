91f5735d1b2af48dc493fb5cf67092b27650a2ec
Jack Wright <jack.wright@aimlab.com>
2021-09-09 09:25:12 +1000

    drivers: flash: wip flash emulator driver
    added first working version of flash emulator driver
    flash_emu stores data on an eeprom device, provided via devicetree
    the current implementation is a basic wrapper around the eeprom api
    the erase function should be modified to use a static page buffer
    
    Signed-off-by: Jack Wright <jack.wright@aimlab.com>

--- drivers/flash/flash_emulator.c
+++ drivers/flash/flash_emulator.c
@@ -7,8 +7,18 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER( flash_emulator, CONFIG_FLASH_LOG_LEVEL );
 
+<<<<<<< HEAD
 #define DEV_CONFIG(dev) ((dev)->config)
 
+=======
+#define DEV_DATA(dev) ((dev)->data)
+#define DEV_CONFIG(dev) ((dev)->config)
+
+struct flash_emu_data {
+	// TODO: remove if not required
+};
+
+>>>>>>> b1314fd614 (drivers: flash: wip flash emulator driver)
 struct flash_emu_config {
 	/* FLASH size */
 	size_t size;
@@ -19,9 +29,12 @@ struct flash_emu_config {
 
 	/* The EEPROM device used to emulate the FLASH */
 	const struct device *eeprom_dev;
+<<<<<<< HEAD
 
 	const size_t erase_page_size;
 	const uint8_t *erase_page;
+=======
+>>>>>>> b1314fd614 (drivers: flash: wip flash emulator driver)
 };
 
 static const struct flash_parameters flash_emu_parameters =
@@ -33,13 +46,23 @@ static const struct flash_parameters flash_emu_parameters =
 
 int flash_emu_read(const struct device *dev, off_t offset, void *data, size_t len)
 {
+<<<<<<< HEAD
 	const struct flash_emu_config *dev_config = DEV_CONFIG( dev );
 
 	return eeprom_read( dev_config->eeprom_dev, offset, data, len );
+=======
+	// struct flash_emu_data *dev_data = DEV_DATA( dev );
+	const struct flash_emu_config *dev_config = DEV_CONFIG( dev );
+
+	return eeprom_read( dev_config->eeprom_dev, offset, data, len );
+
+    // return -1;
+>>>>>>> b1314fd614 (drivers: flash: wip flash emulator driver)
 }
 
 int flash_emu_write(const struct device *dev, off_t offset, const void *data, size_t len)
 {
+<<<<<<< HEAD
 	const struct flash_emu_config *dev_config = DEV_CONFIG( dev );
 
 	return eeprom_write( dev_config->eeprom_dev, offset, data, len );
@@ -64,10 +87,19 @@ static size_t limit_write_count(size_t pagesize,
 	}
 
 	return count;
+=======
+	// struct flash_emu_data *dev_data = DEV_DATA( dev );
+	const struct flash_emu_config *dev_config = DEV_CONFIG( dev );
+
+	return eeprom_write( dev_config->eeprom_dev, offset, data, len );
+
+    // return -1;
+>>>>>>> b1314fd614 (drivers: flash: wip flash emulator driver)
 }
 
 int flash_emu_erase(const struct device *dev, off_t offset, size_t size)
 {
+<<<<<<< HEAD
 	const struct flash_emu_config *dev_config = DEV_CONFIG( dev );
 	int err = 0;
 
@@ -88,6 +120,21 @@ int flash_emu_erase(const struct device *dev, off_t offset, size_t size)
 	size_t last_page_len = offset + size - next_page;
 	err = eeprom_write( dev_config->eeprom_dev, next_page, dev_config->erase_page, last_page_len );
 
+=======
+	// struct flash_emu_data *dev_data = DEV_DATA( dev );
+	const struct flash_emu_config *dev_config = DEV_CONFIG( dev );
+	int err = -ENOSR;
+	
+	// FIXME: use an iterator or something other than malloc
+	uint8_t *eraseValues = (uint8_t*)k_malloc( size );
+	if( eraseValues )
+	{
+		// write the block of erase values, let the underlying eeprom driver handle the semantics
+		memset( eraseValues, flash_emu_parameters.erase_value, size );
+		err = eeprom_write( dev_config->eeprom_dev, offset, eraseValues, size );
+	}
+
+>>>>>>> b1314fd614 (drivers: flash: wip flash emulator driver)
     return err;
 }
 
@@ -114,6 +161,10 @@ void flash_emu_page_layout(const struct device *dev, const struct flash_pages_la
 
 static int flash_emu_init( const struct device *dev )
 {
+<<<<<<< HEAD
+=======
+	// struct flash_emu_data *dev_data = DEV_DATA(dev);
+>>>>>>> b1314fd614 (drivers: flash: wip flash emulator driver)
 	const struct flash_emu_config *dev_config = DEV_CONFIG(dev);
 
 	if( !device_is_ready( dev_config->eeprom_dev ))
@@ -136,12 +187,19 @@ static const struct flash_driver_api flash_emu_api = {
 #endif /* CONFIG_FLASH_PAGE_LAYOUT */
 };
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> b1314fd614 (drivers: flash: wip flash emulator driver)
 #define FLASH_EMU_INIT(n) \
 	enum {								     \
 		INST_##n##_BYTES = (DT_INST_PROP_BY_PHANDLE(n, eeprom, size) / 8),	     \
 		INST_##n##_PAGES = (INST_##n##_BYTES / DT_INST_PROP_BY_PHANDLE(n, eeprom, pagesize)),	     \
 	}; \
+<<<<<<< HEAD
 	static const uint8_t flash_emu_##n##_erase_page[DT_INST_PROP_BY_PHANDLE(n, eeprom, pagesize)] = {[0 ... DT_INST_PROP_BY_PHANDLE(n, eeprom, pagesize)-1] = flash_emu_parameters.erase_value}; \
+=======
+>>>>>>> b1314fd614 (drivers: flash: wip flash emulator driver)
 	static const struct flash_emu_config flash_emu_##n##_config = { \
 		.size = DT_INST_PROP_BY_PHANDLE(n, eeprom, size), \
 	IF_ENABLED(CONFIG_FLASH_PAGE_LAYOUT, (			     \
@@ -150,11 +208,18 @@ static const struct flash_driver_api flash_emu_api = {
 			.pages_size  = DT_INST_PROP_BY_PHANDLE(n, eeprom, pagesize), \
 		},))						     \
 		.eeprom_dev = DEVICE_DT_GET(DT_INST_PHANDLE(n, eeprom)), \
+<<<<<<< HEAD
 		.erase_page_size = ARRAY_SIZE(flash_emu_##n##_erase_page), \
 		.erase_page = flash_emu_##n##_erase_page, \
 	}; \
 	DEVICE_DT_INST_DEFINE(n, &flash_emu_init, \
 		NULL, NULL, \
+=======
+	}; \
+	static struct flash_emu_data flash_emu_##n##_data; \
+	DEVICE_DT_INST_DEFINE(n, &flash_emu_init, \
+		NULL, &flash_emu_##n##_data, \
+>>>>>>> b1314fd614 (drivers: flash: wip flash emulator driver)
 		&flash_emu_##n##_config, POST_KERNEL, \
 		CONFIG_FLASH_EMULATOR_INIT_PRIORITY, &flash_emu_api); \
 
